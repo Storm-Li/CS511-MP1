2eaa9b40e250abfd233c17429577329be1e74b47
root <root@sp17-cs511-08.cs.illinois.edu>
2017-02-25 15:54:47 -0600

    disable transparent hugepage

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 sudo cp mongodb-org-3.4.repo /etc/yum.repo.d/
 sudo yum install -y mongodb-org
+sudo cp disable-transparent-hugepages /etc/init.d/disable-transparent-hugepages
+sudo chmod 755 /etc/init.d/disable-transparent-hugepages
+sudo chkconfig --add disable-transparent-hugepages
+sudo mkdir /etc/tuned/no-thp
+sudo cp tuned.conf /etc/tuned/no-thp/tuned.conf
+sudo tuned-adm profile no-thp
+cat /sys/kernel/mm/transparent_hugepage/enabled
+cat /sys/kernel/mm/transparent_hugepage/defrag
--- remove.sh
+++ remove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo service mongod stop
-sudo yum erase $(rpm -qa | grep mongodb-org)
+sudo yum erase -y $(rpm -qa | grep mongodb-org)
 sudo rm -r /var/log/mongodb
 sudo rm -r /var/lib/mongo
