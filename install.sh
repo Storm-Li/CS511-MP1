b9af75205a63e44bf4f5b3a5cc1bccb139a06898
root <root@sp17-cs511-08.cs.illinois.edu>
2017-02-26 11:11:27 -0600

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp mongodb-org-3.4.repo /etc/yum.repo.d/
+sudo cp mongodb-org-3.4.repo /etc/yum.repo.d/mongodb-org-3.4.repo
 sudo yum install -y mongodb-org
 sudo cp disable-transparent-hugepages /etc/init.d/disable-transparent-hugepages
 sudo chmod 755 /etc/init.d/disable-transparent-hugepages
@@ -9,3 +9,5 @@ sudo cp tuned.conf /etc/tuned/no-thp/tuned.conf
 sudo tuned-adm profile no-thp
 cat /sys/kernel/mm/transparent_hugepage/enabled
 cat /sys/kernel/mm/transparent_hugepage/defrag
+sudo service mongod start
+mongoimport --db cs511 --collection restaurants --file restaurants.json
